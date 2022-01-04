cad624b08705cfffa4bd7e29a45ad1eadeab99de
GeorgeOprian <49019556+GeorgeOprian@users.noreply.github.com>
2022-01-04 21:13:15 +0200

    Missing files added

---
+++ pizzabuono/src/main/java/com/delivery/pizzabuono/PizzabuonoApplication.java
@@ -0,0 +1,13 @@
+package com.delivery.pizzabuono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzabuonoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PizzabuonoApplication.class, args);
+    }
+
+}
---
+++ pizzabuono/src/test/java/com/delivery/pizzabuono/PizzabuonoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delivery.pizzabuono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzabuonoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
