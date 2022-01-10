12bf0dcfe6275bb758e2059712d95d2fc069276e
GeorgeOprian <49019556+GeorgeOprian@users.noreply.github.com>
2022-01-10 20:39:29 +0200

    Before evaluation

--- Tema - Relatie many to many/src/main/resources/db/migration/V3__course_creation.sql
+++ Tema - Relatie many to many/src/main/resources/db/migration/V3__course_creation.sql
@@ -4,3 +4,15 @@ CREATE TABLE course (
     room INT NOT NULL,
     PRIMARY KEY (id)
 );
+
+CREATE TABLE user_course (
+    user_id BIGINT NOT NULL,
+    course_id BIGINT NOT NULL,
+    PRIMARY KEY (user_id, course_id)
+);
+
+ALTER table user_course
+ADD constraint FK_USER FOREIGN KEY (user_id) REFERENCES users (id);
+
+ALTER table user_course
+ADD constraint FK_COURSE FOREIGN KEY (course_id) REFERENCES course (id);
